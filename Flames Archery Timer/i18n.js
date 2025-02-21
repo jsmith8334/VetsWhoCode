27e8e9ce2ba3b37bfdf072e873d3220d3ce8cb3f
Jonathan <JSmith@8334@gmail.com>
2025-02-20 21:14:46 -0500

    added archery timer that I made and finished out the simple html page

---
+++ Flames Archery Timer/i18n.js
@@ -0,0 +1,18 @@
+var i18n = {
+    enUS: {
+        "176cd88e-ad14-4a8b-9633-c8257ad35d92": "Start / Continue",
+        "2e507705-99cb-4d92-8fa7-436e32b67083": "Emergency Stop",
+        "78e23400-3a88-4271-9310-e2af36837c8d": "How to use the timer",
+        "ec50e48a-2cfd-4b19-bdbc-161aef491f82": "Click on {%AB} or {%CD} to toggle which line of archers shoots first.",
+        "17a7b876-4cae-4032-ad70-f1b0331afa8b": "Leave blank if there is just one line of archers.",
+        "64141455-4ad9-4968-ae2a-2cdb1d6f893b": "Click on {%RemainingTime} to change the time limit (10-240 seconds).",
+        "b6f9447d-542f-497f-b006-0a09ef6743e0": "Click on {%StartContinue} to start or advance the timer.",
+        "91eafab3-1e35-488d-a24e-f5c5c9cc96c7": "Click on {%EmergencyStop} to halt the timer immediately.",
+        "f1f31ca2-56f0-4a86-a92a-2a1ab120058b": "Continue to the timer »",
+        "ac1cb77e-01fd-4432-a185-01053bd5b611": "Accept and continue »",
+        "6e7c53cb-4dd0-4376-92ea-d6392a309be4": "{%Seconds}s",
+        "1caf0e11-f363-4555-9c97-dd0b9877e483": "All",
+        "2337ff0f-9840-44fd-8ac3-529034132604": "Toggle main display controls",
+        "03baa248-d587-4e20-88dd-93d95172b82a": "Show/hide tenths of seconds"
+    },
+}
